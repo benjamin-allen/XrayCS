92edcf5835019ca8eeb3d4e06cb5de3452557b10
benjamin-allen <beallen529@gmail.com>
2019-04-05 16:58:58 -0500

    Remove test for entity returning copies since it now returns references, and add additional tests.

--- UnitTests/EntityTest.cs
+++ UnitTests/EntityTest.cs
@@ -16,21 +16,30 @@ namespace UnitTests
         }
 
         [TestMethod]
+        public void BasicConstructor()
+        {
+            Assert.AreEqual(entity.NumRegisteredComponents, 0);
+            Assert.AreEqual(entity.NumComponents, 0);
+            Assert.AreNotEqual(entity.MaxComponents, 0);
+        }
+
+        [TestMethod]
+        public void ArgumentConstructor()
+        {
+            entity = new Entity(2);
+            Assert.AreEqual(entity.NumRegisteredComponents, 0);
+            Assert.AreEqual(entity.NumComponents, 0);
+            Assert.AreEqual(entity.MaxComponents, 2);
+        }
+
+        [TestMethod]
         public void AddIncrementsComponentCount()
         {
             entity.Add<A>();
-            Assert.AreEqual(entity.NumComponents, 1);
+            Assert.AreEqual(entity.NumRegisteredComponents, 1);
             entity.Add<B>();
+            Assert.AreEqual(entity.NumRegisteredComponents, 2);
             Assert.AreEqual(entity.NumComponents, 2);
-            try
-            {
-                entity.Add<B>();
-                Assert.AreEqual(true, false);
-            }
-            catch(ArgumentException)
-            {
-                Assert.AreEqual(entity.NumComponents, 2);
-            }
         }
 
         [TestMethod]
@@ -41,11 +50,11 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void AddWithComponentCopy()
+        public void AddWithPreconstructedComponent()
         {
             A a = new A();
-            A copy = entity.Add<A>(a);
-            Assert.AreNotEqual(Object.ReferenceEquals(a, copy), true);
+            var reference = entity.Add<A>(a);
+            Assert.AreEqual(a, reference);
         }
 
         [TestMethod]
@@ -57,10 +66,29 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void AllowDoubleAddingComponentIfUnsetThrowOnError()
+        public void EnsureDoubleAddingHasNoSideEffects()
         {
             entity.Add<A>();
-            entity.Add<A>(throwOnError = false);
+            try
+            {
+                entity.Add<A>();
+                Assert.Equals(false, true);
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual(entity.NumComponents, 1);
+                Assert.AreEqual(entity.NumRegisteredComponents, 1);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Entity allowed addition of too many components")]
+        public void PreventRegistrationOfTooManyComponents()
+        {
+            entity = new Entity(2);
+            entity.Add<A>();
+            entity.Add<B>();
+            entity.Add<C>();
         }
     }
 }
